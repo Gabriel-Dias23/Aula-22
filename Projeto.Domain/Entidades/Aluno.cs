2de8a9003c4387ae4b32d7ab98eefc915d58cf76
Gabriel-Dias23 <Bieldias074@gmail.com>
2025-10-22 20:19:36 -0300

    comit 1

---
+++ Projeto.Domain/Entidades/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+  namespace Projeto.Domain.EnTidads
+{
+    internal class Aluno
+    {
+        public Aluno(string nome, string cPF, string rA, string eMAIL)
+        {
+            this.nome = nome;
+            CPF = cPF;
+            RA = rA;
+            EMAIL = eMAIL;
+        }
+
+        public string nome { get; private set; }
+        public string CPF { get;  private set; }
+        public string RA { get; private set; }
+        public string EMAIL { get; private set; }
+    }
+}
+
+
+
+
